a6c671836766c2710dfaec130e3a4bf7879ac13e
Abel AR <abel_john_14@hotmail.com>
2021-06-24 00:01:37 -0500

    Finish with array classes, modificated elements inside the array

--- fundamentals-of-typescript-class001/dist/type-tuple.js
+++ fundamentals-of-typescript-class001/dist/type-tuple.js
@@ -7,3 +7,22 @@ console.log('username', newUser[1]);
 console.log('newUser.length', newUser.length);
 console.log('username.length', newUser[1].length);
 console.log('id', newUser[0]);
+// TUPLAS CON VARIOS VALORES
+// id, username, isUserPro
+var newUserInformation;
+newUserInformation = [2, "Regulus", true];
+console.log("newUserInformation", newUserInformation);
+// ARREGLO DE TUPLAS
+var arrayTuple;
+arrayTuple = [];
+arrayTuple.push([1, "Shion"]);
+arrayTuple.push([2, "Aldebaran"]);
+arrayTuple.push([3, "Mani"]);
+var newArrayTuple = arrayTuple.sort();
+console.log("arrayTuple", arrayTuple);
+console.log("newArrayTuple", newArrayTuple);
+// USO DE FUNCIONES EN UN ARRAY
+// "Manigoldo" ---> "Aspros"
+arrayTuple[2][1] = arrayTuple[2][1].concat("goldo");
+console.log("arrayTuple", arrayTuple);
+console.log("newArrayTuple", newArrayTuple);
--- fundamentals-of-typescript-class001/src/type-tuple.ts
+++ fundamentals-of-typescript-class001/src/type-tuple.ts
@@ -7,3 +7,31 @@ console.log('username', newUser[1]);
 console.log('newUser.length', newUser.length);
 console.log('username.length', newUser[1].length);
 console.log('id', newUser[0]);
+
+// TUPLAS CON VARIOS VALORES
+// id, username, isUserPro
+
+let newUserInformation: [number, string, boolean];
+newUserInformation = [2, "Regulus", true];
+
+console.log("newUserInformation", newUserInformation);
+
+// ARREGLO DE TUPLAS
+
+let arrayTuple: [number, string][];
+arrayTuple = [];
+arrayTuple.push([1, "Shion"]);
+arrayTuple.push([2, "Aldebaran"]);
+arrayTuple.push([3, "Mani"]);
+
+const newArrayTuple = arrayTuple.sort();
+console.log("arrayTuple", arrayTuple);
+console.log("newArrayTuple", newArrayTuple);
+
+// USO DE FUNCIONES EN UN ARRAY
+// "Manigoldo" ---> "Aspros"
+
+arrayTuple[2][1] = arrayTuple[2][1].concat("goldo");
+
+console.log("arrayTuple", arrayTuple);
+console.log("newArrayTuple", newArrayTuple);
